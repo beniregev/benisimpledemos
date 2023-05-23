4ec0cd71b5f719aed005569f36f4d8a379b06a04
BinyaminRegev <beniregev@gmail.com>
2023-05-23 15:02:53 +0300

    Refactoring

--- src/main/java/com/beniregev/demos/StringToCharSequenceDeno.java
+++ src/main/java/com/beniregev/demos/StringToCharSequenceDeno.java
@@ -3,11 +3,20 @@ package com.beniregev.demos;
 import org.apache.commons.lang3.StringUtils;
 
 public class StringToCharSequenceDeno {
-    public static void main(String[] args) {
+
+    public void stringNotBlank() {
         String notBlank = "string";
         String blankString = "";
 
         System.out.println("String \"" + notBlank + "\" is" + (StringUtils.isNotBlank(notBlank) ? " not " : " ") +"blank");
         System.out.println("String \"" + blankString + "\" is" + (StringUtils.isNotBlank(blankString) ? " not " : " ") +"blank");
+    }
+
+
+    public static void main(String[] args) {
+        StringToCharSequenceDeno demo = new StringToCharSequenceDeno();
+
+        demo.stringNotBlank();
+
     }
 }
